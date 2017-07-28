63cb6c5d497528b41832eda0b1b0e98382eeff3c
kevinthesun <kevinthesunwy@gmail.com>
2017-07-28 00:49:05 -0700

    Refactor dcgan model definition

--- model_def/dcgan.py
+++ model_def/dcgan.py
@@ -1,5 +1,5 @@
-import mxnet as mx
-
+from mxnet.gluon.nn import Dense, Activation, Conv2D, Conv2DTranspose, \
+    BatchNorm, LeakyReLU, Flatten, Sequential
 
 class DCGAN(object):
     """DCGAN model definition
@@ -10,76 +10,62 @@ class DCGAN(object):
         self.num_channel = num_channel
         self.num_layer = num_layer
 
-    def make_generator(self, z_latent, no_bias=True, fix_gamma=True, eps=1e-5 + 1e-12):
+    def make_generator(self, use_bias=False, scale=False, epsilon=1e-5 + 1e-12):
         ngf = self.num_g_filter
         nc = self.num_channel
         nlayer = self.num_layer
+        self.generator = Sequential()
 
-        def trans_conv_factory(data, index, kernel, stride=(1, 1), pad=(0, 0), num_filter=ngf, apply_bn=True,
-                               act_type='relu'):
-            gen = mx.sym.Deconvolution(data, name='g' + str(index), kernel=kernel, stride=stride,
-                                       pad=pad, num_filter=num_filter, no_bias=no_bias)
-            gen_bn = mx.sym.BatchNorm(gen, name='gbn' + str(index), fix_gamma=fix_gamma, eps=eps)
-            gen_act = mx.sym.Activation(gen_bn if apply_bn else gen, name='gact' + str(index),
-                                        act_type=act_type)
-            return gen_act
-
-        gin = trans_conv_factory(z_latent, index=1, kernel=(4, 4), num_filter=ngf * 2 ** nlayer)
-        last_out = gin
-
+        self._trans_conv_factory(self.generator, kernel=(4, 4), num_filter=ngf * 2 ** nlayer)
         for idx in range(nlayer):
-            last_out = trans_conv_factory(last_out, index=idx + 2, kernel=(4, 4), stride=(2, 2), pad=(1, 1),
+            self._trans_conv_factory(self.generator, kernel=(4, 4), strides=(2, 2), padding=(1, 1),
                                      num_filter=ngf * 2 ** (nlayer - idx - 1))
 
-        gout = trans_conv_factory(last_out, index=nlayer + 2, kernel=(4, 4), stride=(2, 2), pad=(1, 1),
-                                  num_filter=nc, apply_bn=False, act_type='tanh')
-        return gout
+        self._trans_conv_factory(self.generator, kernel=(4, 4), strides=(2, 2), padding=(1, 1),
+                                 num_filter=nc, apply_bn=False, act_type='tanh')
 
-    def make_discriminator(self, image, no_bias=True, fix_gamma=True, eps=1e-5 + 1e-12):
+    def make_discriminator(self, use_bias=False, scale=False, epsilon=1e-5 + 1e-12):
         ndf = self.num_d_filter
         nlayer = self.num_layer
+        self.discriminator = Sequential()
 
-        def conv_factory(data, index, kernel, stride=(1, 1), pad=(0, 0), num_filter=ndf, apply_bn=True):
-            disc = mx.sym.Convolution(data, name='d' + str(index), kernel=kernel, stride=stride,
-                                       pad=pad, num_filter=num_filter, no_bias=no_bias)
-            disc_bn = mx.sym.BatchNorm(disc, name='dbn' + str(index), fix_gamma=fix_gamma, eps=eps)
-            disc_act = mx.sym.LeakyReLU(disc_bn if apply_bn else disc, name='dact' + str(index),
-                                         act_type='leaky', slope=0.2)
-            return disc_act
-
-        din = conv_factory(image, index=1, kernel=(4, 4), stride=(2, 2), pad=(1, 1), apply_bn=False)
-        last_out = din
-
+        self._conv_factory(self.discriminator, kernel=(4, 4), strides=(2, 2), padding=(1, 1),
+                           apply_bn=False)
         for idx in range(nlayer):
-            last_out = conv_factory(last_out, index=idx + 2, kernel=(4, 4), stride=(2, 2), pad=(1, 1),
-                                num_filter=ndf * 2 ** (idx + 1))
+            self._conv_factory(self.discriminator, kernel=(4, 4), strides=(2, 2), padding=(1, 1),
+                               num_filter=ndf * 2 ** (idx + 1))
 
-        dout = mx.sym.Convolution(last_out, name='d5', kernel=(4, 4), num_filter=1, no_bias=no_bias)
-        dout = mx.sym.Flatten(dout)
-        return dout
+        self.discriminator.add(Conv2D(channels=1, kernel=(4, 4), use_bias=use_bias))
+        self.discriminator.add(Flatten())
 
-    def make_predictor(self, image, num_dim, no_bias=True, fix_gamma=True, eps=1e-5 + 1e-12):
+    def make_predictor(self, num_dim, use_bias=False, scale=False, epsilon=1e-5 + 1e-12):
         ndf = self.num_d_filter
         nlayer = self.num_layer
+        self.predictor = Sequential()
 
-        def conv_factory(data, index, kernel, stride=(1, 1), pad=(0, 0), num_filter=ndf, apply_bn=True):
-            pred = mx.sym.Convolution(data, name='d' + str(index), kernel=kernel, stride=stride,
-                                      pad=pad, num_filter=num_filter, no_bias=no_bias)
-            pred_bn = mx.sym.BatchNorm(pred, name='dbn' + str(index), fix_gamma=fix_gamma, eps=eps)
-            pred_act = mx.sym.LeakyReLU(pred_bn if apply_bn else pred, name='dact' + str(index),
-                                        act_type='leaky', slope=0.2)
-            return pred_act
-
-        din = conv_factory(image, index=1, kernel=(4, 4), stride=(2, 2), pad=(1, 1), apply_bn=False)
-        last_out = din
-
+        self._conv_factory(self.predictor, kernel=(4, 4), strides=(2, 2), padding=(1, 1),
+                           apply_bn=False)
         for idx in range(nlayer):
-            last_out = conv_factory(last_out, index=idx + 2, kernel=(4, 4), stride=(2, 2), pad=(1, 1),
-                                    num_filter=ndf * 2 ** (idx + 1))
+            self._conv_factory(self.predictor, kernel=(4, 4), strides=(2, 2), padding=(1, 1),
+                               num_filter=ndf * 2 ** (idx + 1))
 
-        pout = mx.sym.Convolution(last_out, name='d5', kernel=(4, 4), num_filter=1, no_bias=no_bias)
-        flatten = mx.sym.Flatten(pout)
-        fc = mx.sym.FullyConnected(flatten, num_hidden=num_dim)
-        pact = mx.sym.Activation(fc, act_type='tanh')
-        return pact
+        self.predictor.add(Conv2D(channels=1, kernel=(4, 4), use_bias=use_bias))
+        self.predictor.add(Flatten())
+        self.predictor.add(Dense(units=num_dim, activation='tanh'))
+
+    def _trans_conv_factory(self, model, kernel, strides=(1, 1), padding=(0, 0),
+                            num_filter=self.num_d_filter, apply_bn=True, act_type='relu'):
+        model.add(Conv2DTranspose(channels=num_filter, kernel=kernel, strides=strides,
+                                      pad=padding, use_bias=use_bias))
+        if apply_bn:
+            model.add(BatchNorm(scale=scale, epsilon=epsilon))
+        model.add(Activation(activation=act_type))
+
+    def _conv_factory(self, model, kernel, strides=(1, 1), padding=(0, 0), num_filter=self.num_d_filter,
+                      apply_bn=True):
+        model.add(Conv2D(channels=num_filter, kernel=kernel, strides=strides,
+                                 padding=padding, use_bias=use_bias))
+        if apply_bn:
+            model.add(BatchNorm(scale=scale, epsilon=epsilon))
+        model.add(LeakyReLU(alpha=0.2))
 
